ce1c20996b8949255a60f44b0281e9b1fb0e25c4
Hariz Hakim <harizhakim85@gmail.com>
2024-11-17 08:34:53 +0800

    deploy on scroll

--- src/contract/contract.js
+++ src/contract/contract.js
@@ -1,4 +1,4 @@
 import TrafficViolation from "./TrafficViolation.json"
 
-export const CONTRACT_ADDRESS = "0xe7E64f18F2345427d588e3fca0d6340b91047ebF"
+export const CONTRACT_ADDRESS = "0x5F36A20D3D06B7bfc63a59c3A566489907938F5c"
 export const CONTRACT_ABI = TrafficViolation;
